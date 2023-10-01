0b6c06d2b9feb82f02bd2d5bffd743b43464bce3
Franco Kaufmann <87050811+FrancoKaufmann@users.noreply.github.com>
2023-10-01 18:03:47 -0300

    I modified the script and latoun mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,16 @@
-var numeroTelefone = "5521973739881";
+let numeroTelefone = "5521973739881";
 
+// Mais pedidos
 function maisPedidos01() {
   let pedidoNome01 = document.querySelector('#pedidoNome01');
 
-  var mensagem = `Olá, gostaria de pedir o combo: ${pedidoNome01.textContent}!`;
+  let mensagem = `Olá, gostaria de pedir o combo: ${pedidoNome01.textContent}!`;
   
   // Formate o número de telefone removendo caracteres especiais e espaços.
   numeroTelefone = numeroTelefone.replace(/\D/g, '');
 
   // Crie o link do WhatsApp com o número de telefone e a mensagem.
-  var urlWhatsApp = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
+  let urlWhatsApp = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
 
   // Redirecione o usuário para o link do WhatsApp.
   window.location.href = urlWhatsApp;
@@ -45,6 +46,53 @@ function maisPedidos03() {
   window.location.href = urlWhatsApp;
 }
 
+// Combos +Familia
+function combo01() {
+  let comboNome01 = document.querySelector('#comboNome01');
+
+  var mensagem = `Olá, gostaria de pedir o combo: ${comboNome01.textContent}!`;
+  
+  // Formate o número de telefone removendo caracteres especiais e espaços.
+  numeroTelefone = numeroTelefone.replace(/\D/g, '');
+
+  // Crie o link do WhatsApp com o número de telefone e a mensagem.
+  var urlWhatsApp = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
+
+  // Redirecione o usuário para o link do WhatsApp.
+  window.location.href = urlWhatsApp;
+}
+
+function combo02() {
+  let comboNome02 = document.querySelector('#comboNome02');
+
+  var mensagem = `Olá, gostaria de pedir o combo: ${comboNome02.textContent}!`;
+  
+  // Formate o número de telefone removendo caracteres especiais e espaços.
+  numeroTelefone = numeroTelefone.replace(/\D/g, '');
+
+  // Crie o link do WhatsApp com o número de telefone e a mensagem.
+  var urlWhatsApp = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
+
+  // Redirecione o usuário para o link do WhatsApp.
+  window.location.href = urlWhatsApp;
+}
+
+function combo03() {
+  let comboNome03 = document.querySelector('#comboNome03');
+
+  var mensagem = `Olá, gostaria de pedir o combo: ${comboNome03.textContent}!`;
+  
+  // Formate o número de telefone removendo caracteres especiais e espaços.
+  numeroTelefone = numeroTelefone.replace(/\D/g, '');
+
+  // Crie o link do WhatsApp com o número de telefone e a mensagem.
+  var urlWhatsApp = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagem);
+
+  // Redirecione o usuário para o link do WhatsApp.
+  window.location.href = urlWhatsApp;
+}
+
+// Botão principal
 function peçaAgora() {
     var mensagem = "Olá, gostaria de pedir um Frango Assado! poderia me enviar o cardápio?";
     
@@ -56,4 +104,4 @@ function peçaAgora() {
   
     // Redirecione o usuário para o link do WhatsApp.
     window.location.href = urlWhatsApp;
-  }
+}
